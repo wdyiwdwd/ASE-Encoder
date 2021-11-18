e2a9df682e2e6443b0e3e176e0f981a78f3a78ef
wdyiwdwd <573353663@qq.com>
2021-11-18 11:05:42 +0800

    Adjust files

---
+++ build/CMakeFiles/isepure.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../lib/libisepure.pdb"
+  "../lib/libisepure.so"
+  "CMakeFiles/isepure.dir/src/ISEEncoder.cpp.o"
+  "CMakeFiles/isepure.dir/src/ISEPrepocessor.cpp.o"
+  "CMakeFiles/isepure.dir/src/ISETree.cpp.o"
+  "CMakeFiles/isepure.dir/src/ISEUtils.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/isepure.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
